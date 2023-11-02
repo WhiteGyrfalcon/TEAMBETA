4234557b88aba0667101605ba5d5972ebd0901f4
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-11-02 10:30:30 +0200

    create user entity properties

--- InternetGameBlog/InternetGameBlog.Data.Models/User.cs
+++ InternetGameBlog/InternetGameBlog.Data.Models/User.cs
@@ -2,6 +2,17 @@
 {
 	public class User
 	{
+        public User()
+        {
+            this.Games = new HashSet<Game>();
+        }
 
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string ProfilePicUrl { get; set; } = null!;
+
+        ICollection<Game> Games { get; set; }
 	}
 }
