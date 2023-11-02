37b98b9964fb16ec29bf7483eee9e0ae60b475cd
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-11-02 09:58:25 +0200

    Create entity validation constants for game and user entities

--- InternetGameBlog/InternetGameBlog.Common/EntityValidationConstants.cs
+++ InternetGameBlog/InternetGameBlog.Common/EntityValidationConstants.cs
@@ -2,6 +2,28 @@
 {
 	public static class EntityValidationConstants
 	{
+		public static class Game
+		{
+			public const int NameMinLength = 1;
+			public const int NameMaxLength = 1;
 
+			public const int CreatorCompanyMinLength = 1;
+			public const int CreatorCompanyMaxLength = 1;
+
+			public const int PlatformMinLength = 1;
+			public const int PlatformMaxLength = 1;
+		}
+
+		public static class User
+		{
+			public const int PasswordMinLength = 6;
+			public const int PasswordMaxLength = 100;
+
+			public const int FirstNameMinLength = 2;
+			public const int FirstNameMaxLength = 50;
+
+			public const int LastNameMinLength = 3;
+			public const int LastNameMaxLength = 50;
+		}
 	}
 }
