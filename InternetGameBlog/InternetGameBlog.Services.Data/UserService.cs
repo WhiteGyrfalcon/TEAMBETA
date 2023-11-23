2cf0ac330e40be7f393279dffe13bf5d26bb4307
radostin60 <radostindimitrov05@pmgkk.com>
2023-11-23 10:33:09 +0200

    gyza mi

---
+++ InternetGameBlog/InternetGameBlog.Services.Data/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetGameBlog.Services.Data
+{
+    internal class UserService
+    {
+    }
+}
