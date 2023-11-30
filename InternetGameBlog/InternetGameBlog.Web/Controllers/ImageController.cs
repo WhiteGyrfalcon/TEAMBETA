8b5ce223fc07a7e862b3d38c268a97708544c0d7
NickTGH <nik123424@abv.bg>
2023-11-30 10:16:43 +0200

    Added controllers
    Added controllers for all tables in the database

---
+++ InternetGameBlog/InternetGameBlog.Web/Controllers/ImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternetGameBlog.Web.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InternetGameBlog/InternetGameBlog.Web/Controllers/PlatformController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternetGameBlog.Web.Controllers
+{
+    public class PlatformController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
