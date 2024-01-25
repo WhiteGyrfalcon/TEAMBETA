ec0451669899efd31deed73b5d69b215b765934b
NickTGH <nik123424@abv.bg>
2024-01-25 08:58:01 +0200

    added simple views and some more functions

---
+++ InternetGameBlog/InternetGameBlog.Web.ViewModels/Platform/EditPlatformViewModel.cs
@@ -0,0 +1,16 @@
+﻿using InternetGameBlog.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetGameBlog.Web.ViewModels.Platform
+{
+    public class EditPlatformViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<GamePlatform> Games { get; set; }
+    }
+}
